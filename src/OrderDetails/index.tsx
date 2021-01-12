f4eb1e30a53bff18b0b71f794b4f0ec0ff5b0733
Jonatas B. Soares <jonatas.soares@gmail.com>
2021-01-12 10:04:04 -0300

    App Finish

--- src/OrderDetails/index.tsx
+++ src/OrderDetails/index.tsx
@@ -1,10 +1,57 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, Linking, StyleSheet, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { confirmDelivery } from '../api';
+import Header from '../Header';
+import OrderCard from '../OrderCard';
+import { Order } from '../types';
 
-export default function OrderDetails() {
+type Props = {
+  route: {
+    params: { order: Order; }
+  }
+}
+
+export default function OrderDetails({ route }: Props) {
+  const { order } = route.params;
+  const navigation = useNavigation();
+
+  const handleOnCancel = () => {
+      navigation.navigate('Orders');
+  }
+
+  const handleConfirmDelivery = () => {
+    confirmDelivery(order.id)
+      .then(() => {
+        Alert.alert(`Pedido Nº ${order.id} confirmado com sucesso!`);
+        navigation.navigate('Orders');
+      })
+      .catch(() => {
+        Alert.alert(`Houve um erro ao tentar confirmar o pedido Nº ${order.id}`);
+      });
+  }
+
+  const handleStartNavigation = () => {
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`);
+  }
+  
   return (
-    <View style={styles.container}>
-    </View>
+    <>
+      <Header />
+      <View style={styles.container}>
+        <OrderCard order={order}/>
+        <RectButton style={styles.button} onPress={handleStartNavigation}>
+          <Text style={styles.buttonText}>INICIAR NAVEGAÇÃO</Text>
+        </RectButton>
+        <RectButton style={styles.button} onPress={handleConfirmDelivery}>
+          <Text style={styles.buttonText}>CONFIRMAR ENTREGA</Text>
+        </RectButton>
+        <RectButton style={styles.button} onPress={handleOnCancel}>
+          <Text style={styles.buttonText}>CANCELAR</Text>
+        </RectButton>
+      </View>
+    </>
   );
 }
 
--- src/Orders/index.tsx
+++ src/Orders/index.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
-import { StyleSheet, ScrollView } from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, Alert, Text } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard';
+import { Order } from '../types';
 
 export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetchOrders()
+      .then(response => setOrders(response.data))
+      .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
+      .finally(() => setIsLoading(false));
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      fetchData();
+    }
+  }, [isFocused]);
+
+  const handleOnPress = (order: Order) => {
+    navigation.navigate('OrderDetails', { order });
+  }
+
   return (
     <>
       <Header />
       <ScrollView style={styles.container}>
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
-        <OrderCard />
+        {isLoading ? (
+          <Text style={styles.textLoad}>Buscando pedidos...</Text>
+        ) : (orders.map(order => (
+          <TouchableWithoutFeedback key={order.id} onPress={() => handleOnPress(order)}>
+            <OrderCard order={order}/>
+          </TouchableWithoutFeedback>
+        )))}
       </ScrollView>
     </>
   );
@@ -21,5 +51,15 @@ const styles = StyleSheet.create({
   container: {
     paddingRight: '5%',
     paddingLeft: '5%',
-  }
+  },
+  textLoad: {
+    paddingBottom: 15,
+    paddingTop: 15,
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#263238',
+    fontFamily: 'OpenSans_700Bold',
+  },
 });
